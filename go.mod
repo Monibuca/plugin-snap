7505168bece8f0feb065480bf1260ab2104ea36a
dwdcth <dwdcth@163.com>
2022-04-08 20:56:59 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/Monibuca/plugin-snap
+
+go 1.18
+
+require (
+	github.com/Monibuca/engine/v3 v3.5.1
+	github.com/Monibuca/utils/v3 v3.0.6
+)
+
+require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef // indirect
+	github.com/cnotch/ipchub v1.1.0 // indirect
+	github.com/funny/slab v0.0.0-20180511031532-b1fad5e5d478 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/rtp v1.7.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/q191201771/naza v0.19.1 // indirect
+	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+package snap
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+	"os/exec"
+	"strconv"
+	"time"
+
+	. "github.com/Monibuca/engine/v3"
+	. "github.com/Monibuca/utils/v3"
+
+	"github.com/Monibuca/utils/v3/codec"
+)
+
+type SnapProcess = func(*Stream) ([]byte, error)
+
+var snapKind = map[string]SnapProcess{
+	"iframe": snapIFrame,
+	"rtmp":   snapStream,
+	"rtsp":   snapStream,
+	"flv":    snapStream,
+	"hls":    snapStream,
+}
+
+func init() {
+
+	pc := PluginConfig{
+		Name:   "Snap",
+		Config: &struct{}{},
+		// Version: "v3.0.0",
+	}
+	pc.Install(nil)
+	http.HandleFunc("/api/snap", snap)
+}
+
+func snap(w http.ResponseWriter, r *http.Request) {
+	CORS(w, r)
+	timeout := r.URL.Query().Get("timeout")
+	t, err := strconv.Atoi(timeout)
+	if timeout == "" || t == 0 || err != nil {
+		t = 2500
+	}
+	kind := r.URL.Query().Get("kind")
+	if kind == "" {
+		kind = "iframe"
+	}
+	snapProc, ok := snapKind[kind]
+	if !ok {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("kind param is wrong"))
+		return
+	}
+
+	if streamPath := r.URL.Query().Get("stream"); streamPath != "" {
+		s := FindStream(streamPath)
+		if s == nil {
+			w.WriteHeader(http.StatusNotAcceptable)
+			w.Write([]byte("stream not found"))
+			return
+		}
+		data, err := snapWithTimeOut(s, snapProc, t)
+		if err != nil {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.Header().Add("Content-Type", "image/jpeg")
+		w.Write(data)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("stream param is empty"))
+	}
+}
+
+func snapWithTimeOut(s *Stream, snap SnapProcess, timeout int) ([]byte, error) {
+
+	after := time.After(time.Duration(timeout) * time.Millisecond)
+	var data []byte
+	var err error
+L:
+	for {
+		select {
+		case <-after:
+			if err == nil {
+				err = errors.New("timeout")
+			}
+			break L
+		default:
+			data, err = snap(s)
+			if err == nil {
+				break L
+			}
+		}
+	}
+	return data, err
+}
+
+func snapIFrame(s *Stream) ([]byte, error) {
+
+	if v := s.WaitVideoTrack(); v != nil {
+		pack := v.IDRing.Value.(*AVItem).Value.(*VideoPack)
+		header := *v.ExtraData
+		buf := bytes.NewBuffer(nil)
+		for _, h := range header.NALUs {
+			buf.Write(codec.NALU_Delimiter2)
+			buf.Write(h)
+		}
+
+		buf.Write(codec.NALU_Delimiter2)
+		buf.Write(pack.NALUs[0])
+
+		cmd := exec.Command("ffmpeg", "-i", "pipe:0", "-vframes", "1", "-f", "mjpeg", "pipe:1")
+		cmd.Stdin = bytes.NewReader(buf.Bytes())
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			return nil, err
+		}
+		return out.Bytes(), nil
+	} else {
+		return nil, errors.New("stream no track")
+	}
+}
+
+func snapStream(s *Stream) ([]byte, error) {
+
+	return nil, errors.New("not implement yet")
+}
